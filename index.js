b50e202dca6839c0628ecf2728fb5c70c3403bc5
nirmalpro <nirmalsrivastava1000@gmail.com>
2020-11-27 17:37:27 +0530

    nirml first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//let name='mummy';
+//let firstName;
+//let age=30;
+//let selectedcolor=null;
+//let isApproved = false;
+//const interestRate = 1; 
+//interestRate = 3;
+//console.log(interestRate);
+//let person ={
+//name:'mash',
+//age:30
+//};
+//person.name='john';
+//let selection = 'name';
+//person[selection] = 'mary'
+//console.log(person.name);
+//let selectedColors = ['red','blue'];
+//selectedColors[2] = 'green'
+//console.log(selectedColors.length);
+///function greet(name,lastname){
+//    console.log("hello "+ name+" "+lastname);
+//}
+//greet("john","smith");
+function square(number){
+    return number*number;
+}
+let number = square(2);
+console.log(number);
+
